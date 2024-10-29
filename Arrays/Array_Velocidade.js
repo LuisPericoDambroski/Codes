9f40c1c64e8b24fef5b67b1f0a4986d50ada5164
Luis Dambroski <luispericodambroski@gmail.com>
2024-10-28 22:32:43 -0300

    Add files via upload

---
+++ Arrays/Array_Velocidade.js
@@ -0,0 +1,16 @@
+
+var array= []
+var num
+
+for(var i=1;i<=6;i++){
+    num= parseInt(prompt("digite um numero"))
+    array.push(num)
+    
+    document.write(`${num}<br>`)
+   
+}
+
+var invertidoArray = [...array].reverse();
+
+document.write(`Array normal ${array}<br>`)
+document.write(`Array invertido: ${invertidoArray}<br>`);
---
+++ Arrays/array.js
@@ -0,0 +1,10 @@
+var linguagens= ["java","python","C#"]
+linguagens.push("html")
+document.write(`${linguagens}<br>`)
+linguagens.splice(1,1,"portugol","PHP","C++")
+document.write(`${linguagens}<br>`)
+document.write(`${linguagens.length}<br>`)
+linguagens.splice(2,1,"SQL","Swift","Go")
+document.write(`${linguagens}<br>`)
+linguagens.splice(2,1,"python")
+document.write(linguagens.indexOf("Swift"))
